d3f0c15bd82c374d47d1be6700b227af6a533a5e
Lucas Costa <lucas.co090869@gmail.com>
2024-06-29 15:01:02 -0300

    chore: create entities

--- CatalogV2.Domain/Entities/Category.cs
+++ CatalogV2.Domain/Entities/Category.cs
@@ -1,6 +1,37 @@
-﻿namespace CatalogV2.Domain.Entities
+﻿using CatalogV2.Domain.Validation;
+
+namespace CatalogV2.Domain.Entities
 {
-    public class Category
+    public sealed class Category : Entity
     {
+        public string Name { get; private set; }
+        public string ImageUrl { get; private set; }
+        public ICollection<Product> Products { get; private set; }
+
+        public Category(string name, string imageUrl)
+        {
+            ValidateDomain(name, imageUrl);
+        }
+
+        public Category(int id, string name, string imageUrl)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid id.");
+            Id = id;
+            ValidateDomain(name, imageUrl);
+        }
+
+        private void ValidateDomain(string name, string imageUrl)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "The property 'name' is required");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(imageUrl), "The property 'imageUrl' is required");
+
+            DomainExceptionValidation.When(name.Length < 3, "The 'name' has to have at least 3 caracters");
+
+            DomainExceptionValidation.When(imageUrl.Length < 5, "The 'imageUrl' has to have at least 5 caracters");
+
+            Name = name;
+            ImageUrl = imageUrl;
+        }
     }
 }
--- CatalogV2.Domain/Entities/Product.cs
+++ CatalogV2.Domain/Entities/Product.cs
@@ -4,38 +4,43 @@ namespace CatalogV2.Domain.Entities
 {
     public sealed class Product : Entity
     {
-        public string Name { get; set; }
-        public string ImageUrl { get; set; }
-        public ICollection<Product> Products { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public string ImageUrl { get; private set; }
+        public int Stock { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
 
-        public Product(string name, string imageUrl)
+        public void Update(string name, string description, decimal price, string imageUrl, int stock, DateTime createdAt, int categoryId)
         {
-            ValidateDomain(name, imageUrl);
+            ValidateDomain(name, description, price, imageUrl, stock, createdAt);
+            CategoryId = categoryId;
         }
 
-        public Product(int id, string name, string imageUrl)
+        private void ValidateDomain(string name, string description, decimal price, string imageUrl, int stock, DateTime createdAt)
         {
-            DomainExceptionValidation.When(id < 0, "Invalid id.");
-            Id = id;
-            ValidateDomain(name, imageUrl);
-        }
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "The property 'name' is required");
 
-        private void ValidateDomain(string name, string imageUrl)
-        {
-            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
-            "The property 'name' is required");
+            DomainExceptionValidation.When(name.Length < 3, "The 'name' has to have at least 3 caracters");
 
-            DomainExceptionValidation.When(string.IsNullOrEmpty(imageUrl),
-                "The property 'imageUrl' is required");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "The property 'description' is required");
 
-            DomainExceptionValidation.When(name.Length < 3,
-               "The 'name' has to have at least 3 caracters");
+            DomainExceptionValidation.When(description.Length < 5, "The 'name' has to have at least 5 caracters");
 
-            DomainExceptionValidation.When(imageUrl.Length < 5,
-                "The 'imageUrl' has to have at least 5 caracters");
+            DomainExceptionValidation.When(price < 0, "The property 'price' has to be greater than 0");
+
+            DomainExceptionValidation.When(imageUrl?.Length > 250, "The property 'name' cannot exceed 250 caracters");
+
+            DomainExceptionValidation.When(stock < 0, "The property 'stock' has to be greater than 0");
 
             Name = name;
+            Description = description;
+            Price = price;
             ImageUrl = imageUrl;
+            Stock = stock;
+            CreatedAt = createdAt;
         }
     }
 }
