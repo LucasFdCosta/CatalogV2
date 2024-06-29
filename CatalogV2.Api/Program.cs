a052557057df0e6f873baeb96b96a7d3bd81aa1d
Lucas Costa <lucas.co090869@gmail.com>
2024-06-29 16:23:54 -0300

    feat: implement cross cutting layer
    add dependency injection in the DI container

--- CatalogV2.Api/Program.cs
+++ CatalogV2.Api/Program.cs
@@ -1,8 +1,11 @@
+using CatalogV2.CrossCutting.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddInfrastructureApi(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ CatalogV2.CrossCutting/IoC/DependencyInjectionApi.cs
@@ -0,0 +1,30 @@
+﻿using CatalogV2.Application.Interfaces;
+using CatalogV2.Application.Mappings;
+using CatalogV2.Application.Services;
+using CatalogV2.Domain.Interfaces;
+using CatalogV2.Infrastructure.Context;
+using CatalogV2.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CatalogV2.CrossCutting.IoC
+{
+    public static class DependencyInjectionApi
+    {
+        public static IServiceCollection AddInfrastructureApi(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(configuration.GetConnectionString("DefaultConnection"),
+                new MySqlServerVersion(new Version(8, 0, 29))));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            return services;
+        }
+    }
+}
